dbc7293a098164986fb1ba2986f43ae2f340df97
Germano Corrêa <germanobruscato@gmail.com>
2016-06-11 01:05:08 -0300

    Exibir e enviar mensagens implementado

--- cache/view/1cc43fe21b7037f35f1e9d857d674440a1641519_0.file.index.tpl.php
+++ cache/view/1cc43fe21b7037f35f1e9d857d674440a1641519_0.file.index.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-06-07 09:53:41
+/* Smarty version 3.1.29, created on 2016-06-11 01:03:47
   from "/home/sculy/public_html/aula/bate-papo/View/index.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_5756c3d5563c17_29371348',
+  'unifunc' => 'content_575b8da30290d3_55743801',
   'file_dependency' => 
   array (
     '1cc43fe21b7037f35f1e9d857d674440a1641519' => 
     array (
       0 => '/home/sculy/public_html/aula/bate-papo/View/index.tpl',
-      1 => 1465244802,
+      1 => 1465617823,
       2 => 'file',
     ),
   ),
@@ -21,7 +21,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
     'file:_rodape.tpl' => 1,
   ),
 ),false)) {
-function content_5756c3d5563c17_29371348 ($_smarty_tpl) {
+function content_575b8da30290d3_55743801 ($_smarty_tpl) {
 $_smarty_tpl->smarty->ext->_subtemplate->render($_smarty_tpl, "file:_cabecalho.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
@@ -29,22 +29,22 @@ $_smarty_tpl->smarty->ext->_subtemplate->render($_smarty_tpl, "file:_cabecalho.t
 <div class="container">
     <h2>Register Now!</h2>
     <form class="register-form" action="gravar_usuario.php" method="post">
-        <input type="text" name="username" id="username-register" placeholder="Username"><br>
-        <input type="email" name="email" id="email-register" placeholder="Email"><br>
-        <input type="password" name="password" id="password-register" placeholder="Password"><br>
-        <input type="password" name="password-repeat" id="password-repeat-register" placeholder="Repeat your Password"><br>
+        <input type="text" name="username" id="username-register" placeholder="Username" class="input-tosco"><br>
+        <input type="email" name="email" id="email-register" placeholder="Email" class="input-tosco"><br>
+        <input type="password" name="password" id="password-register" placeholder="Password" class="input-tosco"><br>
+        <input type="password" name="password-repeat" id="password-repeat-register" placeholder="Repeat your Password" class="input-tosco"><br>
 
-        <button type="submit" name="register-submit" id="submit-register">Create New User</button>
+        <button type="submit" name="register-submit" id="submit-register" class="botao-tosco">Create New User</button>
     </form>
 </div>
 
 <div class="container">
     <h2>Login</h2>
     <form class="login-form" action="autenticar_usuario.php" method="post">
-        <input type="text" name="username" id="username-login" placeholder="Username"><br>
-        <input type="password" name="password" id="password-login" placeholder="Password"><br>
+        <input type="text" name="username" id="username-login" placeholder="Username" class="input-tosco"><br>
+        <input type="password" name="password" id="password-login" placeholder="Password" class="input-tosco"><br>
 
-        <button type="submit" name="login-submit" id="submit-login">Login</button>
+        <button type="submit" name="login-submit" id="submit-login" class="botao-tosco">Login</button>
     </form>
 </div>
 
